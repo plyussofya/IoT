5d49cef438405efa4cb1dc1d526e6cf49ad3e984
Plyusnina Sofya <114556853+plyussofya@users.noreply.github.com>
2022-10-07 09:05:53 +0800

    Add files via upload

---
+++ SerialCommunicator_auditor/serial_led_photo.py
@@ -0,0 +1,26 @@
+import serial
+import time
+
+lengths = {'s': 3,
+            'u': 0,
+            'd': 0}
+
+def get_connection(port):
+    ser = serial.Serial(port, timeout=1)
+    return ser
+
+def send(ser, message, mesg_len):
+    ser.write(message)
+    time.sleep(0.1)
+    if mesg_len != 0:
+        data = ser.read(mesg_len)
+        result = data.decode()
+        result = result.strip()
+        print(result)
+
+if __name__ == '__main__':
+    ser = get_connection("COM6")
+    while True:
+        inp = input("Enter command:")
+        length = lengths.get(inp, 17)
+        send(ser, inp.encode(), length)
