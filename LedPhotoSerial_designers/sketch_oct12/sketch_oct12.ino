9fa04af9c467ab746e5799b0b7c317d53b728cca
Plyusnina Sofya <114556853+plyussofya@users.noreply.github.com>
2022-12-15 17:06:31 +0800

    Update sketch_oct12.ino

--- LedPhotoSerial_designers/sketch_oct12/sketch_oct12.ino
+++ LedPhotoSerial_designers/sketch_oct12/sketch_oct12.ino
@@ -15,12 +15,12 @@ void loop() {
       else if (message == 'd'){
          digitalWrite(led_pin, LOW);
       }
-      else if (message == 's' || message == 'f'){
+      else if (message == 's'){
          int val = analogRead(sensor_pin);
          val = map(val, 0, 1023, 100, 999);
          Serial.print(val);
       }
-      else if(message != 'p')
+      else{
         Serial.println("Unknown message");
        }
     }                  
