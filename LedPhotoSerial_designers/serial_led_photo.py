d1c6178bab68cf370fb0824863eaba79c6d97085
Plyusnina Sofya <114556853+plyussofya@users.noreply.github.com>
2022-12-15 17:47:46 +0800

    Update serial_led_photo.py

--- LedPhotoSerial_designers/serial_led_photo.py
+++ LedPhotoSerial_designers/serial_led_photo.py
@@ -5,23 +5,59 @@ lengths = {'s': 3,
            'u': 0,
            'd': 0,
            'f': 0}
+values = []
 
 def get_connection(port):
-    ser = serial.Serial(port, timeout=1)
+    ser = serial.Serial(port,timeout=1)
     return ser
 
-def send(ser, message, mesg_len):
+def send(ser,message, mesg_len):
     ser.write(message)
     time.sleep(0.1)
     if mesg_len != 0:
-        data = ser.read(mesg_len)
+        data = ser.readline()
         result = data.decode()
         result = result.strip()
-        print(result)
-
+        return (result)
+    
+    
 if __name__ == '__main__':
     ser = get_connection("COM6")
     while True:
-        inp = input("Enter command:")
-        length = lengths.get(inp, 17)
-        send(ser, inp.encode(), length)
+        print("Введите одну из команд: \nu - turn on, \nd - turn off, \na - on/off according to sensor readings, \nl - enable data transfer, \nf - brightness")      
+        inp = input()
+        if inp == "u" or inp == "d" :
+            send(ser, inp.encode(), lenghts[inp])
+        elif inp == "a":
+            while True:
+                com = "s"
+                val = send(ser, com.encode(), lenghts[com])
+                if val:
+                    val = int(val)
+                    print(val)
+                    if val > 900:
+                        send(ser, 'd'.encode(), 0)
+                    else:
+                        send(ser, 'u'.encode(), 0)
+        elif inp == "l":
+            while True: 
+                com = "s"
+                val = send(ser, com.encode(), lenghts[com])
+                if val:
+                    values.append(val)
+                print (values)
+        elif inp == "f":
+            while True: 
+                com = "s"
+                val = send(ser, com.encode(), lenghts[com])
+                if val:
+                    val = int(val)
+                    print(val)
+                    if val > 900:
+                        send(ser, 'f'.encode(), 0)
+                        brightness = send(ser, '50'.encode(),1)
+                    else:
+                        send(ser, 'f'.encode(), 0)
+                        brightness = send(ser, '255'.encode(),1)
+        else:
+            print("Unknown command. Repeat the input.")
