82d4f899411880626ab224accd73e2ef12e95b1d
Plyusnina Sofya <114556853+plyussofya@users.noreply.github.com>
2022-12-19 23:03:49 +0800

    Update morse_output.ino

--- MorseShieldSoldering/morse_output/morse_output.ino
+++ MorseShieldSoldering/morse_output/morse_output.ino
@@ -1,13 +1,18 @@
 #define DATA_PIN 9
 #define DATA_LEVEL LOW
 #define SPACE_LEVEL HIGH
+
+#define DOT_DURATION 1
 #define DASH_DURATION 3
-#define DOT_DURATION 7
+
+#define LETTER_DURATION 3
+#define WORD_DURATION 7
+
 #define TU 100
 
 String CODES[] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
 char LETTERS[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-int NLETTRERS = 27;
+int NLETTRERS = 26;
 
 void setup() {
   Serial.begin(9600);
@@ -17,14 +22,13 @@ void setup() {
 
 void loop() {
   if(Serial.available() >= 1){
-    char iletter = Serial.read();
-    if (iletter == ' '){
+    char letter = Serial.read();
+    if (letter == ' '){
       send_sep_word();
     }
     else{
       for (int il = 0; il < NLETTRERS; il++){
-        if (iletter == LETTERS[il]){
-          Serial.print(LETTERS[il]);
+        if (letter == LETTERS[il]){
           String code = CODES[il];
           send_letter(code);
         }
@@ -34,12 +38,13 @@ void loop() {
 }
 
 void send_letter(String code) {
+  Serial.println(code);
   int code_len = code.length(); 
   for (int i = 0; i < code_len; i++){
      char symbol = code[i];
      if (symbol == '.'){
        digitalWrite(DATA_PIN, DATA_LEVEL);
-       delay(TU);
+       delay(DOT_DURATION * TU);
       }
       else{
         digitalWrite(DATA_PIN, DATA_LEVEL);
@@ -48,11 +53,11 @@ void send_letter(String code) {
       digitalWrite(DATA_PIN, SPACE_LEVEL);
       delay(TU);
    }
-   delay((DASH_DURATION - 1) * TU);
+   delay((LETTER_DURATION - 1) * TU);
 }
 
 void send_sep_word(){
   digitalWrite(DATA_PIN, SPACE_LEVEL);
-  delay((DOT_DURATION - DASH_DURATION) * TU);
+  delay((WORD_DURATION - LETTER_DURATION) * TU);
   Serial.print(" ");
 }
